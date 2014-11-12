eefda3c15c428813e8d37d0a39dc9397b2bb2fe4
chrisatwebis <chrisatwebis>
2014-11-12 11:24:09 -0500

    update the purchase online webform
    update the purchase online webform

--- sites/all/themes/sunflower/js/sunflower.behaviors.js
+++ sites/all/themes/sunflower/js/sunflower.behaviors.js
@@ -133,6 +133,133 @@
       });
     }
   };
+
+
+
+  function get_total_days() {
+    var all_selected = true;
+    $("#webform-component-travel-dates--effective-date select").each(
+      function(){
+        if($(this).val() == "") {
+          all_selected = false;
+          return all_selected;
+        }
+      }
+    );
+    $("#webform-component-travel-dates--expiry-date select").each(
+      function(){
+        if ($(this).val() == "") {
+          all_selected = false;
+          return all_selected;
+        }
+      }
+    );
+    return all_selected;
+  }
+  
+  function show_duration() {
+    if (get_total_days()) {
+      var effective_year = $(".webform-component--step-2--travel-dates--effective-date .year").val();
+      var effective_day = $(".webform-component--step-2--travel-dates--effective-date .day").val();
+      var effective_month = $(".webform-component--step-2--travel-dates--effective-date .month").val();
+      
+      var expiry_month = $(".webform-component--step-2--travel-dates--expiry-date .month").val();
+      var expiry_day = $(".webform-component--step-2--travel-dates--expiry-date .day").val();
+      var expiry_year = $(".webform-component--step-2--travel-dates--expiry-date .year").val();
+      
+      var minutes = 1000*60;
+      var hours = minutes*60;
+      var days = hours*24;
+      var expiry_date = new Date();
+      expiry_date.setFullYear(expiry_year,expiry_month-1,expiry_day);
+      var effective_date = new Date();
+      effective_date.setFullYear(effective_year,effective_month-1,effective_day);
+      var t = expiry_date.getTime() - effective_date.getTime();
+      total_days = Math.round(t/days)+1;
+
+      if($("#duration_days").length) {
+        $("#duration_days #duration").html(Drupal.t("Duration")+": <b>"+total_days+" "+Drupal.t("days")+" </b>");
+      }
+      else {
+        $(".webform-component--step-2--travel-dates .fieldset-wrapper").append('<span id="duration_days"><span id="duration">'+Drupal.t("Duration")+': <b>'+total_days+" "+Drupal.t("days")+' </b></span></span>');
+      }
+      
+      if (total_days <= 0) {
+        $("#duration_days").addClass("warning");
+      }
+      else {
+        $("#duration_days").removeClass("warning");          
+      }
+      
+      // sum_insured_dedutible_check();
+    }
+    else {
+      if($("#duration_days").length) {
+        $("#duration_days").remove();
+      }
+    }
+  }
+
+  function show_age(event) {
+    var delta = event.data.element;
+    if (birth_date_all_selected(delta)) {
+      var dob_year = $(".webform-component--step-2--traveller-information-insured--insured-"+delta+"--birthday .year").val();
+      var dob_day = $(".webform-component--step-2--traveller-information-insured--insured-"+delta+"--birthday .day").val();
+      var dob_month = $(".webform-component--step-2--traveller-information-insured--insured-"+delta+"--birthday .month").val();
+        
+      var minutes = 1000*60;
+      var hours = minutes*60;
+      var days = hours*24;
+      
+      var dob_date = new Date();
+      dob_date.setFullYear(dob_year,dob_month-1,dob_day);
+      
+      var current_date = new Date();
+      
+      var insured_1_age = 0;
+      
+      var diff = current_date - dob_date;
+
+      var diffdays = diff / days;
+            
+      var insured_1_age = Math.floor(diffdays / 365.25);
+      
+      if ($(".webform-component--step-2--traveller-information-insured--insured-"+delta+" .insured_age").length) {
+        $(".webform-component--step-2--traveller-information-insured--insured-"+delta+" .insured_age .insured_age_duration").html(
+          Drupal.t("Age")+": <b>"+insured_1_age+" "+Drupal.t("year old")+" </b>"
+        );
+      }
+      else {
+        $(".webform-component--step-2--traveller-information-insured--insured-"+delta+" .fieldset-wrapper").append('<span class="insured_age"><span class="insured_age_duration">'+Drupal.t("Age")+': <b>'+insured_1_age+" "+Drupal.t("year old")+' </b></span></span>');
+      }
+
+      if (insured_1_age <= 0) {
+        $(".webform-component--step-2--traveller-information-insured--insured-"+delta+" .insured_age").addClass("warning");
+      }
+      else {
+        $(".webform-component--step-2--traveller-information-insured--insured-"+delta+" .insured_age").removeClass("warning");          
+      }
+
+    }
+    else {
+      if ($(".webform-component--step-2--traveller-information-insured--insured-"+delta+" .insured_age").length) {
+        $(".webform-component--step-2--traveller-information-insured--insured-"+delta+" .insured_age").remove();
+      }
+    }
+  }
+
+  function birth_date_all_selected(delta) {
+    var all_selected = true;
+    $("#webform-component-insurable-members--insured-"+delta+"--birth-date select").each(
+      function(){
+        if ($(this).val() == "") {
+          all_selected = false;
+          return all_selected;
+        }
+      }
+    );
+    return all_selected;
+  }
   
   $( document ).ready(function() {
     //Add close button for mobile menu
@@ -143,7 +270,42 @@
     //purchase online webform
     $("#webform-client-form-184 .webform-component--step-1 > .fieldset-wrapper").append("<div class='continue_btn'><a href='#'>"+Drupal.t("Continue")+"</a></div>");
     $("#webform-client-form-184 .webform-component--step-2 > .fieldset-wrapper").append("<div class='continue_btn'><a href='#'>"+Drupal.t("Continue")+"</a></div>");
-    $("#webform-client-form-184 .webform-component--step-3 > .fieldset-wrapper").append("<div class='continue_btn'><a href='#'>"+Drupal.t("Continue")+"</a></div>");    
+    $("#webform-client-form-184 .webform-component--step-3 > .fieldset-wrapper").append("<div class='continue_btn'><a href='#'>"+Drupal.t("Continue")+"</a></div>");
+    $(".continue_btn").click(function(e){
+      if ($("#webform-client-form-184 .webform-component--step-1").hasClass("collapsed") == false) {
+        $("#webform-client-form-184 .webform-component--step-1").addClass("collapsed").find("> .fieldset-wrapper").hide();
+      }
+      if ($("#webform-client-form-184 .webform-component--step-2").hasClass("collapsed") == false) {
+        $("#webform-client-form-184 .webform-component--step-2").addClass("collapsed").find("> .fieldset-wrapper").hide();
+      }
+      if ($("#webform-client-form-184 .webform-component--step-3").hasClass("collapsed") == false) {
+        $("#webform-client-form-184 .webform-component--step-3").addClass("collapsed").find("> .fieldset-wrapper").hide();
+      }
+      if ($("#webform-client-form-184 .webform-component--step-4").hasClass("collapsed") == false) {
+        $("#webform-client-form-184 .webform-component--step-4").addClass("collapsed").find("> .fieldset-wrapper").hide();
+      }
+      $(this).parents(".webform-component-fieldset").next().removeClass("collapsed").find(" > .fieldset-wrapper").show();
+      var next_header = $(this).parents(".webform-component-fieldset").next().find(" > legend .fieldset-title");
+      // next_header.click();
+      $('html, body').animate({ scrollTop: (next_header.offset().top - 50) }, 1000, function(){});
+      return false;
+    });
+    
+    //purchase online webform custom script
+    var total_days = 0;
+    if($(".webform-component--step-2--travel-dates--effective-date").length > 0){
+      $(".webform-component--step-2--travel-dates--effective-date select").bind("change",show_duration);
+      $(".webform-component--step-2--travel-dates--expiry-date select").bind("change",show_duration);
+      show_duration();
+    }
+
+    //Initilize the default behavior when the page is loaded.   
+    //Since the dob field is well named: ***1***,***2***, so , we can use 1,2,3,4,5 as delta.   
+    $.each([1,2,3,4,5], function(index, value) { 
+      $(".webform-component--step-2--traveller-information-insured--insured-"+value+"--birthday select").bind("change",{element:value},show_age);
+      $(".webform-component--step-2--traveller-information-insured--insured-"+value+"--birthday select").change();
+    });
+    
   });
 
 })(jQuery);
